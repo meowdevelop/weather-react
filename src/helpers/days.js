5c4871a3ea3d6aadbe0c76cb908409430bee0cd3
Nadezhda Nikishina <nadya9449@mail.ru>
2020-02-10 00:24:50 +0400

    Add helpers

---
+++ src/helpers/days.js
@@ -0,0 +1,76 @@
+const days = [          
+    {
+    date: 1581293419000,    
+    cloudiness: 'Ясно',
+    temperatureNight: -15,
+    dayTemperature: -10,
+    snow: false,
+    rain: false
+    },
+    
+    {
+    date: 1581111279000,
+    cloudiness: 'Пасмурно',
+    temperatureNight: -14,
+    dayTemperature: -7,
+    snow: true,
+    rain: false
+    },
+    
+    {
+    date: 1581379819000,
+    cloudiness: 'Ясно',
+    temperatureNight: -17,
+    dayTemperature: -10,
+    snow: false,
+    rain: false
+    },
+    
+    {
+    date: 1581466219000,
+    cloudiness: 'Пасмурно',
+    temperatureNight: -16,
+    dayTemperature: -11,
+    snow: false,
+    rain: false
+    },
+    
+    {
+    date: 1581552619000,    
+    cloudiness: 'Ясно',
+    temperatureNight: -15,
+    dayTemperature: -10,
+    snow: false,
+    rain: false
+    },
+    
+    {
+    date: 1581639019000,
+    cloudiness: 'Пасмурно',
+    temperatureNight: -14,
+    dayTemperature: -7,
+    snow: true,
+    rain: false
+    },
+    
+    {
+    date: 1581725419000,
+    cloudiness: 'Пасмурно',
+    temperatureNight: -17,
+    dayTemperature: -10,
+    snow: true,
+    rain: true
+    },
+    
+    {
+    date: 1581811819000,
+    cloudiness: 'Пасмурно',
+    temperatureNight: -16,
+    dayTemperature: -11,
+    snow: false,
+    rain: true
+    }
+
+    ];
+
+    export default days;
